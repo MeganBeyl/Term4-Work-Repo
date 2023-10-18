9b3864fc5c2c4aa3cc83520bd4d824a1c0e12f8e
Megan Beyl <231206@virtualwindow.co.za>
2023-10-18 13:40:12 +0200

    Starting Form JS

--- scripts/main.js
+++ scripts/main.js
@@ -13,8 +13,16 @@ $(document).ready(function(){
             event.stopPropagation();
         } else {
             //Add any code that should run on submit
+            let signUpInformation = [];
+            let email = document.getElementById("email");
+            let password = document.getElementById("password");
+            
+            let signUpdata = email + password;
+            let data =JSON.stringify(signUpInformation);
+            localStorage.setItem("signUpInformation", data);
             window.location.href= 'pages/browse.html';
         }
         $(this).addClass('was-validated');
     });
 });
+
